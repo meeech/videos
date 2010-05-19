b6e8774dbaf94a7a35215c16e67875f7640c0d9f
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-05-19 00:43:34 -0400

    generate the output file

--- encode.php
+++ encode.php
@@ -6,7 +6,7 @@
  * script to trigger the encoding process.
  * Can be called from CLI or Cron
  * Make executable (hence the #!)
- *
+ * @require php5.3 (though should work in 5.2)
  * @author Mitchell Amihod
  */
 require('php/config.php');
@@ -29,7 +29,7 @@ $db = new NotORM($pdo);
 
 //Debug
 $row = $db->files[13]->update(array('encode'=>1));
-//
+//////
 
 //Basic idea is we're doing a batch at a time so we don't overload server, 
 //And so we dont have worry about how big db gets.
@@ -37,19 +37,43 @@ $file = false;
 do {
     //Look for an item, mark off filesRemaining, and exit if we're done here. 
     $file = $db->files()->where('encode > 0')->limit($config->batchSize)->fetch();
-
-    // $filesRemaining = (bool)$item->count();
     if(!$file) { 
-        echo 'No files remaining';
+        echo "\nNo files remaining";
         continue; 
     }
     //Begin Encoding here.
     if(!file_exists($file['path'])) {
-        echo $file['path'];
+        echo $file['path'] . ' not found.***';
+        //Pipe to log?
+        continue;
     }
+    
+    // VIDEO_TS folders
+    if (is_dir($file['path'])) {
+        $output_file = $file['path'] . '.' . $config->encode_extension;
+    }
+    else {
+        $output_file = dirname($file['path']) . '/' . pathinfo($file['path'],PATHINFO_FILENAME) .'.'.$config->encode_extension;
+        echo $output_file;
+/*
+        $output_file = substr($row->path, 0, strrpos($row->path, '.')) . '.' . $encode_extension;
+        if ($output_file == $row->path) {
+            // Oops!
+            if ($encode_extension == 'amp4') {
+                $output_file = substr($row->path, 0, strrpos($row->path, '.')) . '.m4v';
+            } else {
+                $output_file = substr($row->path, 0, strrpos($row->path, '.')) . '.amp4';
+            }
+        }
+*/
+    }
+    
+    
+    
     $file->update(array('encode'=>0));
     //Debug
-    // var_dump($item->count());
+    echo "\nencoding ". $file['path'];
+
     sleep(1);
 } while ($file);
 
