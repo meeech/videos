7e1a6bab6abcefb7a7f3152ceed0eff99154db20
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-05-20 21:59:37 -0400

    move db connection

--- php/db.php
+++ php/db.php
@@ -3,3 +3,6 @@
 * DB Access
 */
 require('php/notorm/NotORM.php');
+$config = new Config();
+$pdo = new PDO("{$config->dbtype}:dbname={$config->dbname}", $config->username, $config->password);
+$db = new NotORM($pdo);
