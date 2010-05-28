46c14cd179a0bab6bb8288e7cf13fd1b03372a5e
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-05-27 23:12:56 -0400

    clean out crap 

--- views/home.php
+++ views/home.php
@@ -2,18 +2,6 @@
     <?php require 'views/toolbar.php' ?>
     <ul class="rounded">
         <li class="arrow"><a href="views/login.php">Login</a></li>
-        <li class="arrow"><a href="#animations">Animations</a> <small class="counter">8</small></li>
-        <li class="arrow"><a href="#ajax">AJAX</a> <small class="counter">3</small></li>
-        <li class="arrow"><a href="#callbacks">Events</a> <small class="counter">3</small></li>
-        <li class="arrow"><a href="#extensions">Extensions</a> <small class="counter">4</small></li>
-        <li class="arrow"><a href="#video">Video</a></li>
-        <li class="arrow"><a href="#demos">Demos</a> <small class="counter">2</small></li>
-    </ul>
-    <h2>External Links</h2>
-    <ul class="rounded">
-        <li class="forward"><a href="http://www.jqtouch.com/" target="_blank">Homepage</a></li>
-        <li class="forward"><a href="http://www.twitter.com/jqtouch" target="_blank">Twitter</a></li>
-        <li class="forward"><a href="http://code.google.com/p/jqtouch/w/list" target="_blank">Google Code</a></li>
     </ul>
     <ul class="individual">
         <li><a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#100;&#107;&#064;&#109;&#111;&#114;&#102;&#117;&#110;&#107;&#046;&#099;&#111;&#109;">Email</a></li>
