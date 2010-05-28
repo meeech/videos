e6fd3fa76c44be316d91a5e87123ad3c4f5b7ff4
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-05-27 23:26:01 -0400

    set cookie to last 1 year. should be more than sufficient for now.

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ $password = (isset($_POST['password'])) ? md5($_POST['password']): false;
 $result = $db->users->where('username', $username)->where('password', $password)->fetch();
 
 if(false !== $result) {
-    setcookie('videos5_user', $result['username']);
+    setcookie('videos5_user', $result['username'], strtotime('+1 year'));
     require('views/home.php');
 } else {
     $login_errors = 'There was an error with your username or password.';
