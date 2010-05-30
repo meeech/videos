2249c998593a99bb1195b9229aa91d23bf719856
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-05-30 01:51:49 -0400

    bit of a monkey patch to the logout - login title. since we check for cookie i header.php to determine the page, but cookie deletion isn't picked up till next reload. 

--- logout.php
+++ logout.php
@@ -1,6 +1,8 @@
 <?php
 setcookie('videos5_user', '', time() - 3600);
+
 require 'php/header.php';
+$page = 'login';
 $login_errors = 'You have been logged out. Be excellent to each other.';
 require('views/login.php');
 
