9b352faba0b2cc67549888743a061d34396d4bbe
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-05-30 01:53:15 -0400

    fix monkey patch. just typing out the problem helped

--- logout.php
+++ logout.php
@@ -1,8 +1,11 @@
 <?php
+//Delete cookie / cookie info first
 setcookie('videos5_user', '', time() - 3600);
+unset($_COOKIE['videos5_user']);
 
+//then throw in header.php, which can clearly guess the page. 
 require 'php/header.php';
-$page = 'login';
+
 $login_errors = 'You have been logged out. Be excellent to each other.';
 require('views/login.php');
 
