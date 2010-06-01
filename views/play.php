64079359371aae16c0760b361e973c3921130588
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-05-31 22:13:37 -0400

    center it, test an mp4

--- views/play.php
+++ views/play.php
@@ -1,4 +1,7 @@
 <div id="player">
     <?php require 'views/toolbar.php'; ?>
-    <video src="dropbox/glee/test/Glee.S01E12.HDTV.XviD-P0W4.m4v" id="video" autobuffer autoplay controls></video>
+    <center>    
+        <!-- <video id="video" src="dropbox/glee/test/Glee.S01E12.HDTV.XviD-P0W4.m4v" autobuffer autoplay controls></video> -->
+        <video id="video" src="dropbox/redis-key-value-nirvana-small.mp4" autobuffer autoplay controls></video>
+    </center>
 </div>
