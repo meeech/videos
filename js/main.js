bc34b9bc1b3312087f9d43817c541a5c327b9744
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-06-03 18:01:02 -0400

    For some VERY weird reason, addClass/removeClass ISN't working in the global io handlers.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-$(document).ready(function(){
-///////
-$('#jqt').delegate('a.encodeable', 'click', function(e) {
-    //Stop it from bubbling up - which is why we don't use preventDefault 
-    //THat allows the jqtouch code to try to handle it, which then complains about 404
-    // console.log(this);
-    return false;    
-});
-///////
-});
+YUI().use('substitute','node','event','anim', 'io-base','json-parse', function(Y) { Y.on("domready", function() { 
+//BEGIN Y closure
+Y.one('#jqt').delegate('click', function(e) {
+    e.halt();
+    Y.io(this.get('href'), {
+        on: {
+            'success' : function(tId, response, args) {
+                var rText = Y.JSON.parse(response.responseText);
+                console.log(rText);
+            }
+        }
+    });
+
+}, 'a.encodeable');
+
+// Y.on('io:start', function() {
+//     console.log('start');
+//     console.log(Y.one('#spinner').addClass('hide'));
+// });
+// Y.on('io:complete', function() {
+//     //Y.one('#spinner').addClass('util-hide');
+// });
+
+//end closure
+});});
