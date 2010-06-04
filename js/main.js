18369bace344db9fb699e889892a9a63c816cbaf
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-06-03 22:46:54 -0400

    get queue'ing working

--- js/main.js
+++ js/main.js
@@ -2,22 +2,23 @@ YUI().use('substitute','node','event','io-base','json-parse', function(Y) { Y.on
 //BEGIN Y closure
 Y.one('#jqt').delegate('click', function(e) {
     e.halt();
-    Y.io(this.get('href'), {
-        on: {
-            'success' : function(tId, response, args) {
-                var rText = Y.JSON.parse(response.responseText);
-            }
-        }
-    });
+    if(this.hasClass('encodeable')) {
+        Y.io(this.get('href'), {
+            'on': {
+                'success' : function(tId, response, args) {
+                    var rText = Y.JSON.parse(response.responseText);
+                    this.replaceClass('encodeable', 'queued');
+                }
+            },
+            'context': this //THe clicked element
+        });        
+    }
 
-}, 'a.encodeable');
+}, 'ul.videos li a.encodeable,ul.videos li a.queued');
 
-Y.on('io:start', function() {
-    Y.one('#spinner').removeClass('util-hide');
-});
-Y.on('io:complete', function() {
-    Y.one('#spinner').addClass('util-hide');
-});
+//Global IO Event listeners - show/hide spinner
+Y.on('io:start', function() { Y.one('#spinner').removeClass('util-hide'); });
+Y.on('io:complete', function() { Y.one('#spinner').addClass('util-hide'); });
 
 //end closure
 });});
--- queue.php
+++ queue.php
@@ -1,2 +1,31 @@
 <?php
-echo json_encode(1);
+/**
+ * Queue up selected video
+ *
+ * @author Mitchell Amihod
+ */
+require_once 'php/header.php';
+$filepath = (isset($_GET['file'])) ? realpath($_GET['file']) : false;
+
+$result = '';
+switch (true) {
+
+    case (false == $filepath):
+        $result = 'no_filepath';
+    break;
+    
+    case (!file_exists($filepath)):
+        $result = 'file_no_exist';
+    break;
+
+    case ((bool)$db->queue()->where('file LIKE ?',array($filepath))->limit(1)->count()):
+        $result = 'already_queued';
+    break;
+
+    default://Add to the queue
+        $db->queue(array('file'=>$filepath));
+        $result = 'file_queued';
+    break;
+}
+
+echo json_encode(compact('result'));
