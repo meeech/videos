28f61c95d4f47d65712b9f6fe88f6b5547ba9013
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-06-03 22:04:10 -0400

    make yui file local

--- views/header.php
+++ views/header.php
@@ -8,7 +8,9 @@
     <style type="text/css" media="screen">@import "../jqtouch/jqtouch/jqtouch.css";</style> 
     <style type="text/css" media="screen">@import "../jqtouch/themes/apple/theme.css";</style>
     <!-- JS -->
-    <script type="text/javascript" src="http://yui.yahooapis.com/combo?3.1.1/build/yui/yui-min.js&3.1.1/build/json/json-min.js&3.1.1/build/oop/oop-min.js&3.1.1/build/event-custom/event-custom-base-min.js&3.1.1/build/dom/dom-min.js&3.1.1/build/pluginhost/pluginhost-min.js&3.1.1/build/node/node-min.js&3.1.1/build/event/event-min.js&3.1.1/build/querystring/querystring-stringify-simple-min.js&3.1.1/build/queue-promote/queue-promote-min.js&3.1.1/build/datatype/datatype-xml-min.js&3.1.1/build/io/io-min.js"></script>
+    <script src="js/yui.combo.js" type="text/javascript" charset="utf-8"></script>
+    <!-- <script type="text/javascript" src="http://yui.yahooapis.com/combo?3.1.1/build/yui/yui-min.js&3.1.1/build/json/json-min.js&3.1.1/build/oop/oop-min.js&3.1.1/build/event-custom/event-custom-base-min.js&3.1.1/build/dom/dom-min.js&3.1.1/build/pluginhost/pluginhost-min.js&3.1.1/build/node/node-min.js&3.1.1/build/event/event-min.js&3.1.1/build/querystring/querystring-stringify-simple-min.js&3.1.1/build/queue-promote/queue-promote-min.js&3.1.1/build/datatype/datatype-xml-min.js&3.1.1/build/io/io-min.js"></script>-->
+    
     <script type="text/javascript" charset="utf-8">
         var jQT = new $.jQTouch({
             //Be specific about what forms we want jqtouch to handle vs my own handling
