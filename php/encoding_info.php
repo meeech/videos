2052912ca494b2670526942116fefd8bd1744a32
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-06-08 01:00:31 -0400

    Extract out the current encoding frag

---
+++ php/encoding_info.php
@@ -0,0 +1,16 @@
+<?php //@todo extract
+//Fetch Currently Encoding Data - not using file since it doesn't consistently recog line endings
+$logInfo = explode( "\r",file_get_contents($config->encodeLog));
+//Current encoding filename
+preg_match('/ -i \'(.*)\' -o /', current($logInfo), $regs);
+$nowEncoding = array();
+if( !empty($regs) && isset($regs[1]) ) {
+    $nowEncoding['filename'] = $regs[1];
+}
+//If we can extract this, we can poll and update...
+//Maybe also pull ETA?
+$percent = end($logInfo);
+preg_match('/.*, (.*) %/', $percent, $perRegs);
+if(!empty($perRegs) && isset($perRegs[1])) {
+    $nowEncoding['percent'] = $perRegs[1];
+}
--- views/encode_queue.php
+++ views/encode_queue.php
@@ -1,22 +1,5 @@
+<?php require_once 'php/encoding_info.php'; ?>
 <div id="encode-queue">
-    <?php //@todo extract
-    //Fetch Currently Encoding Data - not using file since it doesn't consistently recog line endings
-    $logInfo = explode( "\r",file_get_contents($config->encodeLog));
-    //Current encoding filename
-    preg_match('/ -i \'(.*)\' -o /', current($logInfo), $regs);
-    $nowEncoding = array();
-    if( !empty($regs) && isset($regs[1]) ) {
-        $nowEncoding['filename'] = $regs[1];
-    }
-    //If we can extract this, we can poll and update...
-    //Maybe also pull ETA?
-    $percent = end($logInfo);
-    preg_match('/.*, (.*) %/', $percent, $perRegs);
-    if(!empty($perRegs) && isset($perRegs[1])) {
-        $nowEncoding['percent'] = $perRegs[1];
-    }
-    ?>
-
 <?php require 'views/toolbar.php' ?>
 <ul class="edgetoedge">
     <?php foreach ($db->queue()->order('added ASC') as $qItem): ?>
