c545578113e434bc801a0e50808f213303006af4
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-06-08 02:46:32 -0400

    need to figure out how to stop player. should be on pageanimation out, or some such.

--- views/play.php
+++ views/play.php
@@ -1,8 +1,6 @@
 <div id="player">
     <?php require 'views/toolbar.php'; ?>
-    <center>    
-        <!-- <video id="video" src="dropbox/glee/test/Glee.S01E12.HDTV.XviD-P0W4.m4v" autobuffer autoplay controls></video> -->
+    <center>
         <?= $helper->video($_GET); ?>
-        <!-- <video id="video" src="dropbox/redis-key-value-nirvana-small.mp4" autobuffer autoplay controls></video> -->
     </center>
 </div>
